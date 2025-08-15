c0ceaaca37f2d22609833c149bbdc635a43bf4e2
mauricio0503 <economizemaux@gmail.com>
2025-08-15 15:37:44 -0300

    Fix unescaped characters issue on learn more modal content

--- src/features/cookieBanner/LearnMore/LearnMore.tsx
+++ src/features/cookieBanner/LearnMore/LearnMore.tsx
@@ -11,9 +11,10 @@ export const LearnMore = () => {
         functionality, record interactions, analyze the traffic and use,
         deliver personalized content and ads, and give you the most
         relevant experience by remembering your preferences and repeat
-        visits. Click "Accept" to enable all cookies or "Reject" to
-        disable those cookies that are not categorized as strictly
-        necessary. For more details, please see our Privacy Policy.
+        visits. Click &quot;Accept&quot; to enable all cookies or
+        &quot;Reject&quot; to disable those cookies that are not
+        categorized as strictly necessary. For more details, please see
+        our Privacy Policy.
       </p>
     </div>
   )
